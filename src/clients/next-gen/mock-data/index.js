cd49d414db6f6d525373d4fe082aa26e272aa01c
Amir <amir447613@gmail@gmail.com>
2023-08-18 11:40:56 +0500

    full backend file

---
+++ src/clients/next-gen/mock-data/index.js
@@ -0,0 +1,9 @@
+const { employees,jobs, salaryRanges } = require("./data");
+
+const mockData = {
+  employees: employees,
+  jobs:jobs,
+  salary_ranges:salaryRanges,
+};
+
+exports.mockData = mockData;
