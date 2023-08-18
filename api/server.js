321828d56ae235375793b6c44bf550f03f2d2f8a
Amir <amir447613@gmail@gmail.com>
2023-08-18 12:45:48 +0500

    changed structure

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
--- src/clients/next-gen/mock-data/index.js
+++
@@ -1,9 +0,0 @@
-const { employees,jobs, salaryRanges } = require("./data");
-
-const mockData = {
-  employees: employees,
-  jobs:jobs,
-  salary_ranges:salaryRanges,
-};
-
-exports.mockData = mockData;
